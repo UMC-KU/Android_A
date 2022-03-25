cee639450eec43bc361892cd67d25681dc787d92
lee-june-young <junzero@konkuk.ac.kr>
2022-03-26 05:05:08 +0900

    0326/zero fragment_album 제작

--- zero/app/src/main/java/com/example/flo/AlbumFragment.kt
+++ zero/app/src/main/java/com/example/flo/AlbumFragment.kt
@@ -9,7 +9,7 @@ import com.example.flo.databinding.FragmentAlbumBinding
 
 class AlbumFragment : Fragment() {
     //프래그먼트는 액티비티와 다르게 AppCompatActivity()를 상속받는게 아니라,Fragment() 상속
-    lateinit var binding : FragmentAlbumBinding//바인딩선언
+    lateinit var binding : com.example.flo.databinding.FragmentAlbumBinding//바인딩선언
 
 
     //액티비티의 온크리에이트 => 프레그먼트 온크리에이트뷰
@@ -25,6 +25,8 @@ class AlbumFragment : Fragment() {
         binding.albumBackIv.setOnClickListener {
             (context as MainActivity).supportFragmentManager.beginTransaction().replace(R.id.main_frm,HomeFragment()).commitAllowingStateLoss()
         }
+
+        //binding.songLalacLayout
        return binding.root // fragment_album 뷰의 최상단과 연결되어 있음을 확인할 수 있다.
     }
 }
