47e5d75b1930f653b690b33d35320e370db66a38
dlgusrb3456 <dlgusrb3456@naver.com>
2022-04-02 08:46:55 +0900

    make until fragment detail, before fragment_video

--- Lackms/app/src/main/java/com/example/flo/AlbumFragment.kt
+++ Lackms/app/src/main/java/com/example/flo/AlbumFragment.kt
@@ -9,10 +9,13 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentContainer
 import androidx.fragment.app.FragmentManager
 import com.example.flo.databinding.FragmentAlbumBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 
 class AlbumFragment :Fragment(){
     lateinit var binding:FragmentAlbumBinding
+
+    private val information = arrayListOf("수록곡","상세정보","영상")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -23,6 +26,14 @@ class AlbumFragment :Fragment(){
             (context as MainActivity).supportFragmentManager.beginTransaction().replace(R.id.main_frm,HomeFragment()).commitAllowingStateLoss()
             Toast.makeText(activity,"눌림",Toast.LENGTH_SHORT).show()
         }
+
+        val albumAdapter = AlbumVPAdapter(this)
+        binding.albumMoreContentVp.adapter = albumAdapter
+
+        TabLayoutMediator(binding.albumMoreContentTbl,binding.albumMoreContentVp){
+            tab,position ->
+            tab.text = information[position]
+        }.attach()
         return binding.root
     }
 
