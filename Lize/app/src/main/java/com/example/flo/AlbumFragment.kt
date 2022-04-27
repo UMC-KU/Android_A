d91941a48acdba746ffb9b6f965d837846fc8cc1
scarlett0714 <tpttyakfn03@naver.com>
2022-04-28 03:46:46 +0900

    lalalilala

--- Lize/app/src/main/java/com/example/flo/AlbumFragment.kt
+++ Lize/app/src/main/java/com/example/flo/AlbumFragment.kt
@@ -8,12 +8,15 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.flo.databinding.FragmentAlbumBinding
 import com.google.android.material.tabs.TabLayoutMediator
+import com.google.gson.Gson
 import java.util.zip.Inflater
 
 class AlbumFragment : Fragment() {
     lateinit var binding : FragmentAlbumBinding
 
     private val information = arrayListOf("수록곡", "상세정보", "영상")
+
+    private var gson: Gson = Gson()
 
     //activity : onCreate vs fragment : onCreateView
     override fun onCreateView(
@@ -22,6 +25,11 @@ class AlbumFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentAlbumBinding.inflate(inflater, container, false)
+
+        //argument에서 데이터를 꺼내와서 binding
+        val albumJson = arguments?.getString("album")
+        val album = gson.fromJson(albumJson, Album::class.java)
+        setInit(album) //binding작업 수행
 
         //fragment_album에서 back누르면 다시 HomeFragment로 이동
         binding.albumBackIv.setOnClickListener{
@@ -40,6 +48,12 @@ class AlbumFragment : Fragment() {
         //TabLayoutMediator : tablayout-viewpager2와 연결해주는 중재자
         //tablayout이 선택될 때 해당 viewpager의 위치를 동기화, viewpager 스크롤 시 tablayout 스크롤 위치 동기화
         return binding.root
+    }
+
+    private fun setInit(album: Album){
+        binding.albumImgIv.setImageResource(album.coverImg!!)
+        binding.albumTitleMainTv.text = album.title.toString()
+        binding.albumSingerMainTv.text = album.singer.toString()
     }
 
 
