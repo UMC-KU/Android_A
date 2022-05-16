37a3b7cf29f7071fb8f6e6a2570b4e2e49acf901
JunYoung <uhiccup@gmail.com>
2022-05-17 05:50:44 +0900

    로그인/로그아웃

--- zero/app/src/main/java/com/example/flo/LockerFragment.kt
+++ zero/app/src/main/java/com/example/flo/LockerFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.flo.databinding.FragmentLockerBinding
 import com.google.android.material.tabs.TabLayoutMediator
@@ -33,5 +34,44 @@ class LockerFragment : Fragment() {
             //로그인버튼 누르면 로그인액티비티로 화면 전환
         }
         return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        initViews()
+    }
+    //로그인, 로그아웃 <= 로그인, 로그아웃을 해야하는 상황인지 판단 필요
+    //현재 sharedprf에 jwt값이 저장되어 있는지 확인하기 위해
+    private fun getJwt(): Int {
+        val spf = activity?.getSharedPreferences("auth", AppCompatActivity.MODE_PRIVATE)
+        //activity?를 적어주는 이유는 fragment에서 사용할때 적는 방법이라고 생각해주기
+        return spf!!.getInt("jwt",0) //spf에서 가져온 값이 없다면 0을 반환해줘라는 의미
+    }
+
+    //뷰 초기화시켜주는 함수
+    private fun initViews() {
+        val jwt:Int= getJwt()
+        if(jwt == 0){
+            //로그인을 해야하는 상황
+            binding.lockerLoginTv.text="로그인"
+            binding.lockerLoginTv.setOnClickListener {
+                startActivity(Intent(activity,LoginActivity::class.java)) //로그인화면
+            }
+        }else{
+            binding.lockerLoginTv.text="로그아웃"
+            binding.lockerLoginTv.setOnClickListener {
+                //로그아웃 진행
+                logout()
+                startActivity(Intent(activity,MainActivity::class.java)) //메인화면
+            }
+        }
+    }
+
+    private fun logout(){
+        //jwt를 0으로 만들어주기
+        val spf = activity?.getSharedPreferences("auth",AppCompatActivity.MODE_PRIVATE)
+        val editor = spf!!.edit()
+        editor.remove("jwt") //jwt라는 키값에 저장된 값을 없애준다.
+        editor.apply()
     }
 }
--- zero/app/src/main/java/com/example/flo/LoginActivity.kt
+++ zero/app/src/main/java/com/example/flo/LoginActivity.kt
@@ -3,6 +3,8 @@ package com.example.flo
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.example.flo.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
@@ -15,5 +17,54 @@ class LoginActivity : AppCompatActivity() {
         binding.loginSignUpTv.setOnClickListener {
             startActivity(Intent(this, SignUpActivity::class.java))
         }
+
+        binding.loginSignInBtn.setOnClickListener {
+            login()
+        }
+    }
+
+    //로그인함수
+    private fun login(){
+        //입력하지 않은 부분이 있는지 밸리데이션 체크 필요
+        if (binding.loginIdEt.text.toString().isEmpty()||binding.loginDirectInputEt.text.toString().isEmpty()){
+            Toast.makeText(this, "이메일을 입력해주세요", Toast.LENGTH_SHORT).show()
+            return //함수 작동끝
+        }
+        if(binding.loginPasswordEt.text.toString().isEmpty()){
+            Toast.makeText(this, "비밀번호를 입력해주세요.", Toast.LENGTH_SHORT).show()
+            return //함수의 작동끝
+        }
+
+        val email : String = binding.loginIdEt.text.toString() + "@" + binding.loginDirectInputEt.text.toString()
+        val pwd : String = binding.loginPasswordEt.text.toString()
+        //사용자가 입력한 이메일과 password의 정보가 db에 존재하는지 확인 필요
+        //확인하기 위해서 songbd부터 연결시켜줄께
+
+        val songDB = SongDatabase.getInstance(this)!!
+        val user = songDB.userDao().getUsers(email,pwd)
+
+        user?.let { //렛문안에 있는 코드가 수행되는 경우, db에서 해당유저를 찾았을 경우이다.
+            Log.d("LOGIN_ACT/GET_USER","userId : ${user.id}, $user")
+            saveJwt(user.id)
+            //로그인 다 하면, 메인화면으로 넘어가기
+            startMainActivity()
+        }
+        Toast.makeText(this, "회원 정보가 존재하지 않습니다.", Toast.LENGTH_SHORT).show()
+    }
+
+    //jwt를 저장할수있는함수 <= 이거 왜 만든다고??
+    private fun saveJwt(jwt:Int){
+        //sharedpreference는 또 뭐더라.
+        val spf =getSharedPreferences("auth", MODE_PRIVATE)
+        //에디터는 또 뭐더라 - sharedprf의 짝꿍인가?
+        val editor = spf.edit()
+
+        editor.putInt("jwt",jwt)
+        editor.apply()
+    }
+
+    private fun startMainActivity(){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 }
