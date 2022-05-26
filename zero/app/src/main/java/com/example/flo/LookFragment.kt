106e898c088e2ed849ed6701e548474d8bf5b3aa
JunYoung <uhiccup@gmail.com>
2022-05-26 13:59:25 +0900

    9주차

--- zero/app/src/main/java/com/example/flo/LookFragment.kt
+++ zero/app/src/main/java/com/example/flo/LookFragment.kt
@@ -1,5 +1,6 @@
 package com.example.flo
 
+/*
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,7 +8,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.flo.databinding.FragmentLookBinding
 
-class LookFragment : Fragment() {
+class LookFragment : Fragment(){
 
     lateinit var binding: FragmentLookBinding
 
@@ -20,4 +21,62 @@ class LookFragment : Fragment() {
 
         return binding.root
     }
+}*/
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.flo.databinding.FragmentLookBinding
+
+
+class LookFragment : Fragment(), LookView {
+    private lateinit var binding: FragmentLookBinding
+    private lateinit var floCharAdapter: SongRVAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentLookBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        getSongs()
+    }
+
+    private fun initRecyclerView(result: FloChartResult) {
+        floCharAdapter = SongRVAdapter(requireContext(), result)
+
+        binding.lookFloChartRv.adapter = floCharAdapter
+    }
+
+    private fun getSongs() {
+        val songService = SongService()
+        songService.setLookView(this)
+
+        songService.getSongs()
+
+    }
+
+    override fun onGetSongLoading() {
+        binding.lookLoadingPb.visibility = View.VISIBLE
+    }
+
+    override fun onGetSongSuccess(code: Int, result: FloChartResult) {
+        binding.lookLoadingPb.visibility = View.GONE
+        initRecyclerView(result)
+    }
+
+    override fun onGetSongFailure(code: Int, message: String) {
+        binding.lookLoadingPb.visibility = View.GONE
+        Log.d("LOOK-FRAG/SONG-RESPONSE", message)
+    }
 }
+
